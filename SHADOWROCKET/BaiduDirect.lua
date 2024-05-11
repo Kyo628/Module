e9b6a8b96d1654f37bafdaf18af2138c05588344
Kyo628 <51012973+Kyo628@users.noreply.github.com>
2024-05-11 15:25:13 +0900

    Update BaiduDirect.lua

--- SHADOWROCKET/BaiduDirect.lua
+++ SHADOWROCKET/BaiduDirect.lua
@@ -44,7 +44,7 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ' HTTP/1.1\r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: ' .. host .. '\r\n' ..
                     'Connection: Keep-Alive\r\n\r\n'
         ctx_write(ctx, res)
